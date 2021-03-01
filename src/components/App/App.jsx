854eb9d77b9dc35df2c5d60a3c4ef975df87abea
Jason Lin <jasonwl_1995@yahoo.com>
2021-02-28 23:31:16 -0600

    App.jsx completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
+
+// New imports
+import { HashRouter as Router, Route} from 'react-router-dom';
+
+import FeedbackFeeling from '../FeedbackFeeling/FeedbackFeeling';
+import FeedbackUnderstanding from '../FeedbackUnderstanding/FeedbackUnderstanding';
+import FeedbackSupported from '../FeedbackSupported/FeedbackSupported';
+import FeedbackComments from '../FeedbackComments/FeedbackComments';
+import FeedbackReview from '../FeedbackReview/FeedbackReview';
 
 function App() {
 
@@ -10,6 +19,30 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+
+      <Router>
+        <Route exact path= "/">
+          <FeedbackFeeling />
+        </Route>
+
+        <Route path= "/understanding">
+          <FeedbackFeeling />
+        </Route>
+
+        <Route path= "/support">
+          <FeedbackFeeling />
+        </Route>
+
+        <Route path= "/comment">
+          <FeedbackFeeling />
+        </Route>
+
+        <Route path= "/review">
+          <FeedbackFeeling />
+        </Route>
+
+      </Router>
+
     </div>
   );
 }
